6bda2c12fe7853b36502efd42632f9f3adcae001
mbollo2005 <mbollo2005@gmail.com>
2019-11-07 21:19:42 +0100

    committing HelloWord update in demo.java file

---
+++ demo.java
@@ -0,0 +1,15 @@
+
+
+// Add your imported classes here 
+
+public class HelloWorld
+{
+  Public striing Country = "Cameroun" 
+ 
+ // Add your new constructors  
+
+ public static  main()
+  {
+	 System.out.println("Hello World man !!")
+  }
+}
